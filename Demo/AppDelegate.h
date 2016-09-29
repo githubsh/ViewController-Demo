99f2101abb9a568d5187c177ed21aea499fcf3cc
Miles-Matheson <liyida188@163.com>
2016-09-29 15:17:00 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 厘米科技 on 16/9/29.
+//  Copyright © 2016年 厘米科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
